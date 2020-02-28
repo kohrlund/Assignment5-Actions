1457f5a3fdc42a6b25a9a6357490d02e8dbb1fe4
kohrlund <39636053+kohrlund@users.noreply.github.com>
2020-02-27 19:46:21 -0600

    Test #1

--- task.py
+++ task.py
@@ -2,5 +2,9 @@ def firstrun():
     return "success"
 
 def getArea(radius):
-    area = 3.14159265 * radius * radius
-    return area
+    if(radius >= 0):
+        area = 3.14159 * radius * radius
+        return area
+
+    else:
+        return None
--- tests.py
+++ tests.py
@@ -12,7 +12,17 @@ class TestCase(unittest.TestCase):
         self.assertNotEqual(expected, task.firstrun())
 
     def test3(self):
-        print("Will this run?")
+        #Test radius of 5
+        expected = 5*5*3.14159
+        self.assertEqual(expected, task.getArea(5))
+
+        #Test radius of 0
+        expected = 0
+        self.assertEqual(expected, task.getArea(0))
+
+        #Test radius of -1 (invalid)
+        expected = None
+        assert(task.getArea(-1) is None)
 
 
 if __name__ == '__main__':

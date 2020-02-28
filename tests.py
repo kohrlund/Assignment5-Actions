b9c897fc061a69eb98a174329b74190482a41c89
kohrlund <39636053+kohrlund@users.noreply.github.com>
2020-02-27 19:49:02 -0600

    Update tests.py

--- tests.py
+++ tests.py
@@ -12,15 +12,15 @@ class TestCase(unittest.TestCase):
         self.assertNotEqual(expected, task.firstrun())
 
     def test3(self):
-        #Test radius of 5
+        # Test radius of 5
         expected = 5*5*3.14159
         self.assertEqual(expected, task.getArea(5))
 
-        #Test radius of 0
+        # Test radius of 0
         expected = 0
         self.assertEqual(expected, task.getArea(0))
 
-        #Test radius of -1 (invalid)
+        # Test radius of -1 (invalid)
         expected = None
         assert(task.getArea(-1) is None)
 
